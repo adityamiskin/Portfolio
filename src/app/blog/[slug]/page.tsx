3bcb61987260f0543d582b03bc33394e9ca176cf
adityamiskin <adityamiskin98@gmail.com>
2025-01-12 03:19:52 +0530

    refactor: remove unused blog posts and MDX content files; update blog post structure and Tailwind configuration

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,52 +1,32 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import Img from '@/components/Img';
 import TwitterEmbed from '@/components/TwitterEmbed';
+import { formatDate, getBlogPosts } from '@/lib/utils';
 
 export async function generateStaticParams() {
-	const blogDir = '/src/blogs';
-	let files = fs.readdirSync(path.join(process.cwd(), blogDir));
+	let posts = getBlogPosts();
 
-	files = files.map((file) => file.replaceAll(' ', '-'));
-
-	const paths = files.map((filename) => ({
-		slug: filename.replace('.mdx', ''),
+	return posts.map((post) => ({
+		slug: post.slug,
 	}));
-
-	return paths;
-}
-
-function getPost({ slug }: { slug: string }) {
-	const blogDir = '/src/blogs';
-	const fileName = slug.replaceAll('-', ' ');
-	const markdownFile = fs.readFileSync(
-		path.join(process.cwd(), `${blogDir}/${fileName}.mdx`),
-		'utf-8',
-	);
-
-	const { data: frontMatter, content } = matter(markdownFile);
-
-	return {
-		frontMatter,
-		slug,
-		content,
-	};
 }
 
 const components = { Img, TwitterEmbed };
 
 export default function Post({ params }: any) {
-	const props = getPost(params);
+	let post = getBlogPosts().find((post) => post.slug === params.slug);
+
+	if (!post) {
+		return <div>Post not found</div>;
+	}
 
 	return (
-		<article className='mx-auto my-20 prose md:prose-xl prose-p:text-lg prose-h2:text-7xl prose-headings:mt-4 prose-h2:text-center prose-h3:text-3xl prose-h3:mt-16 dark:prose-invert px-5 prose-img:rounded-md prose-p:mx-auto md:prose-img:w-[1000px] md:prose-img:h-[600px] prose-img:w-full prose-img:h-[200px] font-light md:font-semibold prose-h2:mb-2 prose-h2:font-normal'>
+		<article className='prose dark:prose-invert mx-auto'>
 			<p className='text-sm font-semibold text-center'>
-				{props.frontMatter.date}
+				{formatDate(post.metadata.publishedAt)}
 			</p>
 
-			<MDXRemote source={props.content} components={components} />
+			<MDXRemote source={post.content} components={components} />
 		</article>
 	);
 }
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,29 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
 import BlogWrapper from './blog-wrapper';
+import { getBlogPosts } from '@/lib/utils';
 
 const Blogs = () => {
-	const blogDir = '/src/blogs';
-	const files = fs.readdirSync(path.join(process.cwd(), blogDir));
-
-	const blogs = files.map((filename) => {
-		let fileContent = fs.readFileSync(
-			path.join(process.cwd(), `${blogDir}/${filename}`),
-			'utf-8',
-		);
-		const { data: frontMatter } = matter(fileContent);
-		return {
-			meta: frontMatter,
-			slug: filename.replace('.mdx', '').replaceAll(' ', '-'),
-		};
-	});
+	const blogs = getBlogPosts();
 
 	return (
 		<>
-			{/* <h2 className='fixed top-1/2 w-full text-center text-3xl tracking-wider font-semibold'>
-				COMING SOON...
-			</h2> */}
 			<BlogWrapper blogs={blogs} />
 		</>
 	);
