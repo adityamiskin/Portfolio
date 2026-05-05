04a1f5805334c9f2ccfa496485db6a4f66648c80
adityamiskin <adityamiskin98@gmail.com>
2026-05-05 20:39:07 +0530

    Use server-only Cloudinary env vars

--- src/app/photo/[slug]/page.tsx
+++ src/app/photo/[slug]/page.tsx
@@ -9,9 +9,9 @@ async function getImagesFromFolder(folderPath: string) {
 
   cloudinary.config({
     secure: true,
-    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-    api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
   const results = await cloudinary.api.resources({
